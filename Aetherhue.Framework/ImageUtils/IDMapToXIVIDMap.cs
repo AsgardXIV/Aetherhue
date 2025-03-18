a5f3f3985e69fdfbb81308fe3576688d231b67c2
Asgard <git@asgard.io>
2025-03-18 16:37:14 -0600

    Some cleanup

--- Aetherhue.Framework/ImageUtils/IDMapToXIVIDMap.cs
+++ Aetherhue.Framework/ImageUtils/IDMapToXIVIDMap.cs
@@ -19,9 +19,8 @@ public static class IDMapToXIVIDMap
             for (int x = 0; x < image.Width; x++)
             {
                 var pixel = image[x, y];
-                var withoutAlpha = new Rgb24(pixel.R, pixel.G, pixel.B);
 
-                if (ImageHelpers.ColorsMatch(withoutAlpha, backgroundColor))
+                if (ImageHelpers.ColorsMatch((Color)pixel, backgroundColor, true))
                 {
                     xivIdMap[x, y] = backgroundColor;
                     continue;
@@ -29,23 +28,9 @@ public static class IDMapToXIVIDMap
 
                 int colorIndex = uniqueColors.IndexOf(pixel);
 
-                int row = colorIndex + offset;
-                if(row >= XIVUtils.XIVColorRowCount)
-                {
-                    throw new Exception("Too many unique colors for the selected row offset.");
-                }
+                var (redChannel, greenChannel) = XIVUtils.CalculateXIVRowID(offset, colorIndex, bPack);
 
-                int pair = bPack ? row / 2 : row;
-                if(pair >= XIVUtils.XIVColorPairCount)
-                {
-                    throw new Exception("Too many unique colors for the selected row offset and packing option.");
-                }
-
-                int colorBand = pair * 17;
-                int blend = bPack ? (row % 2 == 0 ? 0 : 255) : pair;
-
-                xivIdMap[x, y] = new Rgba32((byte)colorBand, (byte)blend, (byte)0, (byte)255);
-
+                xivIdMap[x, y] = new Rgba32((byte)redChannel, (byte)greenChannel, (byte)0, (byte)255);
             }
         }
 
--- Aetherhue.Framework/ImageUtils/XIVUtils.cs
+++ Aetherhue.Framework/ImageUtils/XIVUtils.cs
@@ -1,15 +1,29 @@
-﻿namespace Aetherhue.Framework.ImageUtils;
+﻿using SixLabors.ImageSharp.PixelFormats;
+
+namespace Aetherhue.Framework.ImageUtils;
 public static class XIVUtils
 {
     public const int XIVColorRowCount = 32;
     public const int XIVColorPairCount = XIVColorRowCount / 2;
 
-    public static int GetFreeRows(int rowOffset, int uniqueColors, bool bPackRows)
+
+    public static (byte redChannel, byte greenChannel) CalculateXIVRowID(int rowOffset, int targetRow, bool bPackRows)
     {
-        int totalRowCount = bPackRows ? XIVColorRowCount : XIVColorPairCount;
-        int finalOffset = bPackRows ? rowOffset * 2 : rowOffset;
-        int remainingRows = totalRowCount - finalOffset;
-        int requiredRows = remainingRows - uniqueColors;
-        return requiredRows;
+        int row = rowOffset + targetRow;
+        if (row >= XIVColorRowCount)
+        {
+            throw new Exception("Cannot calculate row ID for the selected row offset.");
+        }
+
+        int pair = bPackRows ? row / 2 : row;
+        if (pair >= XIVColorPairCount)
+        {
+            throw new Exception("Cannot calculate row ID for the selected row offset and packing option.");
+        }
+
+        int colorBand = pair * 17;
+        int blend = bPackRows ? (row % 2 == 0 ? 255 : 0) : pair;
+
+        return((byte)colorBand, (byte)blend);
     }
 }
